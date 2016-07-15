a25b35f56361ab7626e282e7ee9824a3f1be0513
Dane Ding <dane.ding@activenetwork.com>
2016-07-16 01:06:24 +0800

    remove usless code

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import DonateContainer from './containers/DonateContainer';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import transitions from './reducers/transitions';
 import currentTransition from './reducers/currentTransition';
 import { Provider } from 'react-redux';
 import './styles/index.scss';
@@ -13,7 +12,6 @@ const initialState = {};
 
 const store = createStore(
     combineReducers({
-      transitions,
       currentTransition
     }),
     initialState,
