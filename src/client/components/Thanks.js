ae637eebfd1f8127ad48ef9e1cffe74a112fac81
Dane Ding <dane.ding@activenetwork.com>
2016-07-17 10:34:43 +0800

    change styles for thanks page

--- src/client/components/Thanks.js
+++ src/client/components/Thanks.js
@@ -5,7 +5,7 @@ export default class Thanks extends React.Component {
     const { donator, currency, amount, timestamp } = this.props.currentTransition;
     return (
       <div className="container donate__thanks">
-        <p className="bg-info">
+        <p className="alert alert-info">
           Thank you very much, <strong>{donator}</strong>. You have donated <strong>{amount}{currency}</strong> in {timestamp}.
           Kindly remind that each hour you can donate once. Thank you for understanding.
         </p>
